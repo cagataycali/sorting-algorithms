bcef07620c5b05bb00ea6852bb883e7b333207a7
cagataycali <cagataycali@icloud.com>
2017-12-21 00:02:49 +0300

    Selection, bubble, insertion added

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -4,9 +4,20 @@ namespace SortingAlgorithms
 {
 	class MainClass
 	{
+		public static void display(int[] arr) 
+		{
+			for (int i = 0; i < arr.Length; i++)
+				Console.WriteLine(arr[i]);
+		}
+
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] arr = { 12, 3, 8, 5, 15, 12, 45, 31 };
+
+			display(arr);
+			//Selection.Sort(arr);
+			//Insertion.Sort(arr);
+			display(arr);
 		}
 	}
 }
